1521cb7eff94876f03d0ead3e763f1fd5dd1f218
Irving <Irving_Zhao@epam.com>
2016-02-25 11:49:13 +0800

    update with DOMReady event

--- js/script.js
+++ js/script.js
@@ -4,42 +4,48 @@ $(function() {
 });
 
 $("#prepend button").click(function() {
-	$("#prepend p").prepend("Yo! ");
+	$("#prepend p").prepend("Yooo! ");
 });
 
 $("#before button").click(function() {
-	
+  $("#before p").before("Yooo! ");
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css("font-size","20px");
 });
 
 $("#css2 button").click(function() {
-	
+  $("#css2 p").css({"font-size":"20px","color": "plum"});
 });
 
 $("#attr1 button").click(function() {
-	
+  var href = $("#attr1 p a").attr("href");
+  console.log(href);
 });
 
 $("#attr2 button").click(function() {
-
+  var href1 = $("#attr2 p a").attr("href","http://www.google.com");
+  console.log(href1);
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
 });
 
 $("#class2 button").click(function() {
-
+  var has = $("#class2 p").hasClass("lead");
+  console.log(has);
 });
 
 $("#form1 button").click(function() {
+  var form1 = $("#exampleInputEmail1").val();
+  console.log(form1);
 
 });
 
 $("#form2 button").click(function() {
+  $("#exampleInputName2").val("Jane Doe is coming");
 
 });
 
